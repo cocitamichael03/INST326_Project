65a4f8d9f980a27304d3d07bb4255c033be75609
cocitamichael03 <65200739+cocitamichael03@users.noreply.github.com>
2021-10-26 14:42:03 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,6 +1,11 @@
 import re
 import urllib.request as url
 
+def calculate_nutrients(weight, height, type):
+    """Find website that takes parameters and returns the total nutrients needed."""
+    web = url.urlopen("website")
+    return 
+
 def scrape():
     our_url = url.urlopen("https://www.livestrong.com/article/132010-general-nutritional-facts-about-chicken/")
 
@@ -16,26 +21,43 @@ class FoodNutrients:
     the default will be 100 grams when searched on the web. (Default weight)
     """
 
-    def __init__(self, food, quantity = 1 , weight = 100):
+    def __init__(self, food, quantity = 1):
+        self.food_lib = {}
         self.food = food
         self.quantity = quantity
 
+    def find_nutrients(self, weight, height):
+        self.weight = weight
+        self.height = height
+        self.type = input("Style: 1, 2, 3")
+        self.total_nutrients = calculate_nutrients(self.weight, self.height, self.type)
+        
+    
+    def add_food(self, food, quantity = 1):
+        self.food = food
+        self.quantity = quantity
+    
     def scrape_web(self):
         for each in self.food:
             pass
 
     def get_total_intake(self):
-        return f""
+        return f"Needed\nTotal fat: {}\nTotal carbs: {}\nTotal protein:\n"
     
     def get_food(self):
         number_items = len(self.food)
         count = 0
         while count < number_items:
-            print(self.food[count])
-        
-        return self.food[0], number_items
+            #print(self.quantity[count])
+            #print(self.food[count])
+            count += 1
+            if self.food[count] not in self.food_lib:
+                self.food_lib[self.food[count]] = self.quantity[count]
+            else:
+                self.food_lib[self.food[count]] += self.quantity[count]
 
-meal1 = FoodNutrients(['egg', 'ham'], 2)
+
+meal1 = FoodNutrients(['egg', 'ham'], [2,1])
 print(meal1.get_food())
 
 
