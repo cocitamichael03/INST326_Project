b95b3317bb9fe7c497b0bb3ca74fbf09bf75d41d
cocitamichael03 <65200739+cocitamichael03@users.noreply.github.com>
2021-10-26 14:08:22 -0400

    Update code.py

--- code.py
+++ code.py
@@ -9,22 +9,34 @@ def scrape():
         line = line.decode("utf-8")
         #print(line)
 
-
-
-
 class FoodNutrients:
-    def __init__(self, food, quantity = 1 , weight = 100)
+    """The FoodNutrients class allows users to input food items
+    and the quantity which is 1 by default.
+    There is also an option for the user to input the weight of the food if know, otherwise,
+    the default will be 100 grams when searched on the web. (Default weight)
+    """
+
+    def __init__(self, food, quantity = 1 , weight = 100):
         self.food = food
-        pass
+        self.quantity = quantity
 
     def scrape_web(self):
-        for each in food:
+        for each in self.food:
             pass
 
-    
-    
     def get_total_intake(self):
         return f""
+    
+    def get_food(self):
+        number_items = len(self.food)
+        count = 0
+        while count < number_items:
+            print(self.food[count])
+        
+        return self.food[0], number_items
+
+meal1 = FoodNutrients(['egg', 'ham'], 2)
+print(meal1.get_food())
 
 
 
