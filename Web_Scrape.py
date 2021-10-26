fe02b835bcb57c623339a80c51efc10bfe4ec712
cocitamichael03 <65200739+cocitamichael03@users.noreply.github.com>
2021-10-26 14:44:46 -0400

    Hi

---
+++ Web_Scrape.py
@@ -0,0 +1,9 @@
+import re
+import urllib.request as url
+
+our_url = url.urlopen("https://www.livestrong.com/article/132010-general-nutritional-facts-about-chicken/")
+
+count = 0
+for line in our_url:
+    line = line.decode("utf-8")
+    print(line)
