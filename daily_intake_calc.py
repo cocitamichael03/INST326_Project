18f347e55e21931784ccb22d5bc39d98f7965d78
cocitamichael03 <65200739+cocitamichael03@users.noreply.github.com>
2021-10-28 11:49:49 -0400

    BMR function added

---
+++ daily_intake_calc.py
@@ -0,0 +1,12 @@
+"""This can be a function that does the calculation or searches the web."""
+
+def get_daily_intake(weight, height, age = False, gender = "Male"):
+    if gender.lower() == "male":
+        height = int(height.split(".")[0])*12 + int(height.split(".")[1])
+        bmr = 66 + (6.3*weight) + (12.9*height) - (6.8*age)
+        print(height)
+
+    if gender.lower() == "female":
+        print("female")
+
+get_daily_intake(100, "2.11")
