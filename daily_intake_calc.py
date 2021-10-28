a432ef075c188e62d6f1dd4f38ead989a605ff6d
cocitamichael03 <65200739+cocitamichael03@users.noreply.github.com>
2021-10-28 18:38:09 -0400

    Update daily_intake_calc.py

--- daily_intake_calc.py
+++ daily_intake_calc.py
@@ -4,9 +4,9 @@ def get_daily_intake(weight, height, age = False, gender = "Male"):
     if gender.lower() == "male":
         height = int(height.split(".")[0])*12 + int(height.split(".")[1])
         bmr = 66 + (6.3*weight) + (12.9*height) - (6.8*age)
-        print(height)
+        print("height:", height, "inches\n",bmr)
 
     if gender.lower() == "female":
         print("female")
 
-get_daily_intake(100, "2.11")
+get_daily_intake(200, "5.11")
